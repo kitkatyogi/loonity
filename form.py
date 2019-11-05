de337724225335c308405e200a3f79a5aee0a92c
kitkatyogi <47849114+kitkatyogi@users.noreply.github.com>
2019-11-05 17:29:36 +0530

    Add files via upload

--- form.py
+++ form.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+from algoliasearch.search_client import SearchClient
+client = SearchClient.create('52ONG898NR', '03ab1cd7b25cef85ba00f6ad75c26954')
+index = client.init_index('single')
 
 @app.route('/')
 def student():
@@ -10,6 +13,15 @@ def result():
    if request.method == 'POST':
       result = request.form
       
+      bookmark_data = {
+         
+         "url": request.form["url"],
+         "tags": request.form["tags"],
+         "description":request.form["desc"]
+
+      }
+     
+      index.save_object(bookmark_data, {'autoGenerateObjectIDIfNotExist': True})
 
       return render_template("result.html",result = result)
 
